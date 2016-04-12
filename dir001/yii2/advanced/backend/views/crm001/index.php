45941a815fe3a6f0c1561647d62a270ddaf34290
Mark <twcloudwebapp@outlook.com>
2016-04-13 04:53:23 +0800

    disable add new row

--- dir001/yii2/advanced/backend/views/crm001/index.php
+++ dir001/yii2/advanced/backend/views/crm001/index.php
@@ -14,7 +14,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Crm001', ['create'], ['class' => 'btn btn-success']) ?>
+        <?php// echo Html::a('Create Crm001', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 <?php Pjax::begin(); ?>    <?= GridView::widget([
         'dataProvider' => $dataProvider,
