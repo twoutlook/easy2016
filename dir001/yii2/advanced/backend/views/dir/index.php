6dd4b7edbe6c3115cab4d368b551b9e836c4c246
Mark <twcloudwebapp@outlook.com>
2016-04-12 13:58:22 +0800

    only allow diradmin to edit

--- dir001/yii2/advanced/backend/views/dir/index.php
+++ dir001/yii2/advanced/backend/views/dir/index.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
+
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\DirSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -10,38 +11,60 @@ use yii\widgets\Pjax;
 $this->title = 'Dirs';
 $this->params['breadcrumbs'][] = $this->title;
 
-$userId=Yii::$app->user->getId();
+$userId = Yii::$app->user->getId();
 //$userId=Yii::$app->user->getId();
-
 //echo "user id is $userId"
-if (!($userId == 3 || $userId == 4)){
-	
-	echo "<h2>You must login to continue!  </h2>";
-	return;
+if (!($userId == 3 || $userId == 4)) { // 3: dir, 4: diradmin
+    echo "<h2>You must login to continue!  </h2>";
+    return;
 }
-
 ?>
 <div class="dir-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+<?php // echo $this->render('_search', ['model' => $searchModel]);  ?>
 
-   
-<?php Pjax::begin(); ?>    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
 
-         //   'id',
-            'dept_name',
-            'job_title',
-            'empe_name',
-            'ext_num',
-             'cell_num',
-             'mailbox',
+<?php Pjax::begin(); ?>   
 
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-<?php Pjax::end(); ?></div>
+    <?php
+    if ($userId == 4) {
+
+        echo GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+                //   'id',
+                'dept_name',
+                'job_title',
+                'empe_name',
+                'ext_num',
+                'cell_num',
+                'mailbox',
+                ['class' => 'yii\grid\ActionColumn'],
+            ],
+        ]);
+    } else {
+
+        echo GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+                //   'id',
+                'dept_name',
+                'job_title',
+                'empe_name',
+                'ext_num',
+                'cell_num',
+                'mailbox',
+//            ['class' => 'yii\grid\ActionColumn'],
+            ],
+        ]);
+    }
+    ?>
+
+
+
+    <?php Pjax::end(); ?></div>
