650e44f145da94bef5fec098a75fbcf46eacc498
Mark <twcloudwebapp@outlook.com>
2016-04-13 05:11:19 +0800

    show crm001 when david login

--- dir001/yii2/advanced/backend/views/site/index.php
+++ dir001/yii2/advanced/backend/views/site/index.php
@@ -1,7 +1,9 @@
 <?php
+
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
+
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\DirSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -9,7 +11,6 @@ use yii\widgets\Pjax;
 $this->title = 'Dirs';
 $this->params['breadcrumbs'][] = $this->title;
 /* @var $this yii\web\View */
-
 ?>
 <div class="site-index">
 
@@ -19,8 +20,21 @@ $this->params['breadcrumbs'][] = $this->title;
         <p class="lead">您已登入富鈦金屬企業內部網站</p>
 
         <p><a class="btn btn-lg btn-success" href="index.php?r=dir%2Findex">打開通信錄</a></p>
-      
+
+        <?php
+        $userId = Yii::$app->user->getId();
+//$userId=Yii::$app->user->getId();
+//echo "user id is $userId"
+        if (($userId == 6)) { // 6: david
+        ?>
+            
+               <p><a class="btn btn-lg btn-success" href="index.php?r=crm001%2Findex">CRM001</a></p>
+        <?php }
+        ?>
+
+
+
     </div>
 
     <div class="body-content">
-</div>
+    </div>
