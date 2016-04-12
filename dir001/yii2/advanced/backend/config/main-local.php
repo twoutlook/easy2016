bed440976aca8f90436dcaacd2c8b52a30d1c9b6
Mark <twcloudwebapp@outlook.com>
2016-04-12 11:24:16 +0800

    edit .gitignore on backend config and web

---
+++ dir001/yii2/advanced/backend/config/main-local.php
@@ -0,0 +1,27 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '1_J8Dkr2Mbq412oKccDWOTBSSouSbYAh',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+		'allowedIPs' => ['185.18.77.102'] // adjust this to your needs
+
+    ];
+}
+
+return $config;
