e9197549cadddb5ac59b33be80304e3ba60a194e
Mark <twcloudwebapp@outlook.com>
2016-04-13 09:03:50 +0800

    show ddl with display name

--- dir001/yii2/advanced/backend/views/book/_form.php
+++ dir001/yii2/advanced/backend/views/book/_form.php
@@ -2,8 +2,9 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use app\models\Author; //2016/4/13 8:49  Class 'Author' not found 
 
-use app\models\Author; //2016/4/13 8:49  Class 'Author' not found ???
+???
 
 
 /* @var $this yii\web\View */
@@ -18,12 +19,15 @@ use app\models\Author; //2016/4/13 8:49  Class 'Author' not found ???
     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'author_id')->textInput() ?>
-        <?= $form->field($model, 'author_id')->dropDownList(\yii\helpers\ArrayHelper::map(Author::find()->select(['id','firstname','lastname'])->all(),'id','firstname'),['class'=>'form-control inline-block']) ?>
+    <?php
+//    $form->field($model, 'author_id')->dropDownList(\yii\helpers\ArrayHelper::map(Author::find()->select(['id', 'firstname', 'lastname'])->all(), 'id', 'firstname'), ['class' => 'form-control inline-block'])
+    $form->field($model, 'author_id')->dropDownList(\yii\helpers\ArrayHelper::map(Author::find()->select(['id', 'firstname', 'lastname'])->all(), 'id', 'displayName'), ['class' => 'form-control inline-block'])
+    ?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+<?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
-    <?php ActiveForm::end(); ?>
+<?php ActiveForm::end(); ?>
 
 </div>
