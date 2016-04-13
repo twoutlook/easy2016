5e696d012b2f5e1e796e2b8a3389681196b92486
Mark <twcloudwebapp@outlook.com>
2016-04-13 18:08:02 +0800

    able to show search, but dept and prj no option

--- dir001/yii2/advanced/backend/views/dir200/index.php
+++ dir001/yii2/advanced/backend/views/dir200/index.php
@@ -24,7 +24,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
+//            'id',
             'dept.dept_name',
             'prj.prj_name',
             'job_title',
