d566ebf1db37fcf1100e11473782115ed27ebb22
Mark <twcloudwebapp@outlook.com>
2016-04-13 16:58:56 +0800

    @var $searchModel app\models\Dir100Search

--- dir001/yii2/advanced/backend/views/dir100/index.php
+++ dir001/yii2/advanced/backend/views/dir100/index.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 
 /* @var $this yii\web\View */
+/* @var $searchModel app\models\Dir100Search */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'Dir100s';
