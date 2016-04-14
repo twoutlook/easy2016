ce62066adcdd17b0ceb03c224aee340b21583ac9
Mark <twcloudwebapp@outlook.com>
2016-04-14 16:27:15 +0800

    turn create and update to Chinese submit

--- dir001/yii2/advanced/backend/views/rfq-material/_form.php
+++ dir001/yii2/advanced/backend/views/rfq-material/_form.php
@@ -21,7 +21,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'seq')->textInput() ?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? '提交' : '提交', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
