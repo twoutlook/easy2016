a69db942f8dc92493421727607af10867849118e
Mark <twcloudwebapp@outlook.com>
2016-04-14 16:31:08 +0800

    turn Update Delete to Chinese respectively, hide ID

--- dir001/yii2/advanced/backend/views/rfq-material/view.php
+++ dir001/yii2/advanced/backend/views/rfq-material/view.php
@@ -15,8 +15,8 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Html::a('修改', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('刪除', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => 'Are you sure you want to delete this item?',
@@ -28,7 +28,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'id',
+//           'id',
             'material',
             'price1',
             'price2',
