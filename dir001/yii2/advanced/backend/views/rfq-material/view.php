ff99ac9930976385d4a3f035d07f6c05ebd0a05a
Mark <twcloudwebapp@outlook.com>
2016-04-14 16:52:06 +0800

    turn are you sure to delete this item to Chinese

--- dir001/yii2/advanced/backend/views/rfq-material/view.php
+++ dir001/yii2/advanced/backend/views/rfq-material/view.php
@@ -19,7 +19,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('刪除', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
+                'confirm' => '您確定要刪除這筆記錄?',
                 'method' => 'post',
             ],
         ]) ?>
